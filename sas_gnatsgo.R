9018cebb911b6e3a3847467c88400d106de977be
Stephen Roecker <stephen.roecker@gmail.com>
2023-04-12 17:22:00 -0500

    Update sas_gnatsgo.R

--- sas_gnatsgo.R
+++ sas_gnatsgo.R
@@ -1,13 +1,22 @@
 
 library(aqp)
 library(soilDB)
+library(sf)
+library(terra)
 
 
-dsn <- "D:/geodata/soils/gNATSGO_CONUS_Oct2022/gNATSGO_CONUS_Oct2022.gdb"
+# load gNATSGO ----
+fp  <- file.path("D:/geodata/soils/gNATSGO_CONUS_Oct2022")
+dsn <- file.path(fp, "gNATSGO_CONUS_Oct2022.gdb")
+
+lyr <- st_layers(dsn)
+
 f <- fetchGDB(dsn, childs = FALSE)
-mu <- get_mapunit_from_GDB(dsn, stats = FALSE)
+mu <- get_mapunit_from_GDB(dsn, stats = FALSE, stringsAsFactors = TRUE)
 # save(f, mu, file = "gnatsgo_Oct22.RData")
 load(file = "gnatsgo_Oct22.RData")
+
+gnatsgo_r <- rast(file.path(fp, "gNATSGO-mukey.tif"))
 
 
 h_avg <- horizons(f) |>
@@ -36,11 +45,14 @@ co_avg <- collapse::collap(
 #   as.data.frame()
 # 
 # co_avg[is.na(co_avg)] <- 0
+# co_avg1 <- co_avg
+# co_avg1[3:6] <- lapply(co_avg[3:6], round, 1) 
 # co_avg2[is.na(co_avg2)] <- 0
+# co_avg21 <- co_avg2
+# co_avg21[3:6] <- lapply(co_avg21[3:6], round, 1) 
 # 
 # sum(co_avg == co_avg2)
 # nrow(co_avg) * ncol(co_avg)
-# 
 # View(co_avg[idx])
 
 
@@ -73,11 +85,156 @@ mu_avg <- within(mu_avg, {
   })
 
 # saveRDS(mu_avg, "mu_avg_sas.rds")
-mu_avg <- readRDS("mu_avg_sas.rds")
+mu_avg <- readRDS(file.path("C:/workspace2", "mu_avg_sas.rds"))
+
+esp <- function(SAR) {
+  (100 * (-0.0126 + 0.01475 * SAR)) / 
+  (1   + (-0.0126 + 0.01475 * SAR))
+}
 
 
-mu_avg$sas030 <- allocate(pH = mu_avg$`ph1to1h2o_r.000-030`, EC = mu_avg$`ec_r.000-030`, SAR = mu_avg$`sar_r.000-030`, to = "FAO Salt Severity")
-mu_avg$sas100 <- allocate(pH = mu_avg$`ph1to1h2o_r.030-100`, EC = mu_avg$`ec_r.030-100`, SAR = mu_avg$`sar_r.030-100`, to = "FAO Salt Severity")
+mu_avg <- within(mu_avg, {
+  ph_030[is.infinite(ph_030)] <- 0
+  ph_100[is.infinite(ph_100)] <- 0
+  
+  ph_h2o     = ph_030
+  ph_030_sat = predict(ph_lm, data.frame(ph_h2o))
+  ph_h2o     = ph_100
+  ph_100_sat = predict(ph_lm, data.frame(ph_h2o))
+                        
+  ESP_030 = esp(`sar_r.000-030`)
+  ESP_100 = esp(`sar_r.030-100`)
+  
+  SAS_030 = allocate(EC = `ec_r.000-030`, pH = ph_030, ESP = ESP_030)
+  SAS_100 = allocate(EC = `ec_r.030-100`, pH = ph_100, ESP = ESP_100)
+  
+  SAS_030x = classCode(saltSeverity(ph = ph_030, ec = `ec_r.000-030`, esp = ESP_030), "saltseverity")
+  SAS_100x = classCode(saltSeverity(ph = ph_030, ec = `ec_r.030-100`, esp = ESP_100), "saltseverity")
+})
 
+ggplot(data.frame(ph1 = mu_avg$`ph1to1h2o_r.000-030`, ph2 = mu_avg$ph_030), aes(ph1, ph2)) + geom_hex(bins = 50) +  scale_fill_binned(colours = viridis::viridis(50, option = "D"), )
+
+table(mu_avg$SAS_030, mu_avg$SAS_030x)
+test <- subset(mu_avg, SAS_030x == "Saline-sodic" & SAS_030 == "slightly sodic")
+
+
+dat <- with(mu_avg, data.frame(
+  mukey = as.integer(mukey), 
+  SAS_030 = as.integer(SAS_030), 
+  SAS_100 = as.integer(SAS_100)
+))
+
+
+
+# rasterize ----
+setwd(fp)
+
+r <- gnatsgo_r
+res <- sqrt(res(r)[1]^2 * ncell(r) / 50)
+test <- rast(res = res, extent = ext(r), crs = crs(r))
+values(test) <- 1:ncell(test)
+plot(test)
+
+makeTiles(gnatsgo_r, test, filename = "gnatsgo_.tif", overwrite = TRUE)
+
+l <- list.files(path = fp, pattern = "gnatsgo_[0-9]{1,2}.tif")
+
+derat <- function(l, dat, vars) {
+  lapply(vars, function(var) {
+    lapply(l, function(x) {
+      cat("processing ", var, x, as.character(Sys.time()), "\n")
+      r <- rast(x)
+      val <- as.integer(values(r))
+      val2 <- data.table::merge.data.table(
+        data.table::data.table(mukey = val), 
+        dat[c("mukey", var)], 
+        by = "mukey", all.x = TRUE, 
+        sort = FALSE
+      )
+      values(r) <- val2[[var]]
+      writeRaster(r, file = gsub(".tif", paste0("_", var, ".tif"), x), overwrite = TRUE)
+  })})
+}
+derat(l, dat, c("SAS_030", "SAS_100"))
+
+l2 <- list.files(path = fp, pattern = "gnatsgo_[0-9]{1,2}_cat.tif", full.names = TRUE)
+
+# lf <- lapply(l2, rast)
+# lf$filename = "test.tif"
+# lf$overwrite = TRUE
+# do.call("merge", lf)
+l2 <- list.files(path = fp, pattern = "gnatsgo_[0-9]{1,2}_SAS_030.tif")
+test <- sprc(l2)
+merge(test, filename = "gnatsgo_SAS_030.tif", overwrite = TRUE)
+
+l3 <- list.files(path = fp, pattern = "gnatsgo_[0-9]{1,2}_SAS_100.tif", full.names = TRUE)
+test <- sprc(l3)
+system.time(merge(test, filename = "gnatsgo_SAS_100.tif", overwrite = TRUE))
+
+
+# gdalUtilities::gdalbuildvrt(gdalfile = unlist(l2), output.vrt = "test.vrt", dryrun = TRUE)
+# gdalUtilities::gdalwarp(srcfile = "test.vrt", dstfile = "test_vrt.tif")
+
+
+
+# GSP maps ----
+fp <- "D:/geodata/project_data/gsp-sas/deliverables/maps/CONUS"
+l <- list.files(fp, pattern = ".tif$", full.names = TRUE)
+r <- rast(c(
+  EC_030  = l[[5]], EC_100  = l[[6]], 
+  ph_030  = l[[3]], pH_100  = l[[4]],
+  ESP_030 = l[[1]], ESP_100 = l[[2]]
+  ))
+
+test <- allocate(
+  EC = values(r$USA840_CONUS_SalinityMap030), 
+  pH = values(r$USA840_CONUS_pHMap030), 
+  ESP = values(r$USA840_CONUS_ESPMap030), 
+  to = "FAO Salt Severity"
+)  
+
+test2 <- r[[1]]
+test2[!is.na(test2)] <- 0
+values(test2) <- as.integer(test)
+writeRaster(test2, filename = "test_dsm.tif")
+
+
+
+# accuracy assessment ----
+load(file = "C:/Users/stephen.roecker/OneDrive - USDA/projects/gsp-sas/LDM-compact_20200709.RData")
+
+s_mps_sf$SAS <- allocate(
+  EC = s_mps_sf$ec_ptf.000_030_cm, 
+  pH = s_mps_sf$ph_ptf.000_030_cm, 
+  ESP = s_mps_sf$esp.000_030_cm
+)
+s_mps_sf$SAS_int <- as.integer(s_mps_sf$SAS)
+write_sf(s_mps_sf, "test.shp")
+
+vars <- c("test2.tif", "test_dsm.tif")
+l  <- file.path(fp, vars)
+
+test2 <- rast(l[1])
+test_dsm <- rast(l[2])
+
+
+ex1 <- extract(test2,    st_transform(s_mps_sf, 5070))
+ex2 <- extract(test_dsm, s_mps_sf)
+ex <- cbind(st_drop_geometry(s_mps_sf), SAS1 = ex1[, 2], SAS2 = ex2[, 2])
+vars <- c("SAS_int", "SAS1", "SAS2")
+ex[vars] <- lapply(ex[vars], function(x) factor(x, levels = 1:11))
+
+
+# SSURGO
+caret::confusionMatrix(table(pred = ex$SAS1, obs = ex$SAS_int))$overall |> round(2) |> format(scientific = FALSE)
+# DSM
+caret::confusionMatrix(table(pred = ex$SAS2, obs = ex$SAS_int))$overall |> round(2) |> format(scientific = FALSE)
+
+
+
+# spot check map units
+f2 <- f[f$mukey == "2611946"]
+h2 <- horizons(f2)
+View(h2[grep("cokey|mukey|ec_|sar_|ph1|^h.0|statsgo", names(h2))])
 
 
